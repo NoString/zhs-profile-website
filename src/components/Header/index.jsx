b71cbfcace24a363fba3773ba04adc2551baa8f2
mrzhssss <mrzhssss@gmail.com>
2023-05-13 19:04:31 +0800

    keep journey

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,24 @@
 import React, {Fragment} from "react";
 import './index.less'
+import {BiPhoneCall} from "react-icons/bi";
 export default () =>{
     return(
         <Fragment>
-            <div className="wrapper">
-                <div className="container">
+            <div className="paddings wrapper">
+                <div className="flexCenter innerWidth container">
                     <div className="name">
                         ZHU HANGSHUAI
                     </div>
+                    <ul className={'menu'}>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Experience</a></li>
+                        <li><a href="">Portfolio</a></li>
+                        <li><a href="">Testimonials</a></li>
+                        <li>
+                            <p>+233 323 323</p>
+                            <BiPhoneCall size={'40px'}/>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </Fragment>
