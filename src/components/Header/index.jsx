1d2b5d4835f605ff314285b0cabeba6e06d74860
mrzhssss <mrzhssss@gmail.com>
2023-05-15 08:23:22 +0800

    keep journey

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,37 @@
 import React, {Fragment} from "react";
 import './index.less'
 import {BiPhoneCall} from "react-icons/bi";
+// npm install framer-motion
+import {motion} from "framer-motion";
+import {headerVariants} from "../../utils/motion";
 export default () =>{
     return(
         <Fragment>
-            <div className="paddings wrapper">
+
+            <motion.div
+                initial="hidden"
+                whileInView="show"
+                variants={headerVariants}
+                viewport={{once:false,amount:0.25}}
+                className="paddings wrapper">
+
                 <div className="flexCenter innerWidth container">
                     <div className="name">
-                        ZHU HANGSHUAI
+                        Z
                     </div>
-                    <ul className={'menu'}>
+                    {/* flexCenter让所有的li居中分布*/}
+                    <ul className={'menu flexCenter'}>
                         <li><a href="">Services</a></li>
                         <li><a href="">Experience</a></li>
                         <li><a href="">Portfolio</a></li>
                         <li><a href="">Testimonials</a></li>
-                        <li>
+                        <li className={'phone flexCenter'}>
                             <p>+233 323 323</p>
                             <BiPhoneCall size={'40px'}/>
                         </li>
                     </ul>
                 </div>
-            </div>
+            </motion.div>
         </Fragment>
     )
 }
